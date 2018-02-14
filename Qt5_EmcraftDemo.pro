3d5d6d2a34c6806c15c6cadd189252d144847757
Alexander Yurtsev <alex@emcraft.com>
2018-02-14 19:33:30 +0300

    Remove Makefile, which is generated by qmake; change app paths

--- Qt5_EmcraftDemo.pro
+++ Qt5_EmcraftDemo.pro
@@ -4,7 +4,7 @@ QT += qml quick
 SOURCES += main.cpp launcher.cpp
 HEADERS += launcher.h
 
-target.path = /opt/Qt5_EmcraftDemo
+target.path = /usr/bin
 qml.files = Qt5_EmcraftDemo.qml content
-qml.path = /opt/Qt5_EmcraftDemo
+qml.path = /usr/share/Qt5_EmcraftDemo
 INSTALLS += target qml
--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ int main(int argc, char* argv[])
     qmlRegisterType<Launcher>("Launcher", 1, 0, "Process");
 
     view.setResizeMode(QQuickView::SizeRootObjectToView);
-    view.setSource(QUrl::fromLocalFile(QCoreApplication::applicationDirPath() +
-		QLatin1String("/Qt5_EmcraftDemo.qml")));
+    view.setSource(QUrl::fromLocalFile(QLatin1String(
+		    "/usr/share/Qt5_EmcraftDemo/Qt5_EmcraftDemo.qml")));
 
     const QString lowerArgument = QString::fromLatin1(argv[1]).toLower();
     if (lowerArgument == QLatin1String("--fullscreen")) {
